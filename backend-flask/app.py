5309c4375bbdcf49639586f9816910dba81fed50
Mikael <mikael.hatanpaa@gmail.com>
2024-09-30 12:06:38 +0200

    implemented xray tracing

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -23,6 +23,10 @@ from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
 
+# xray
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
 # HoneyComb ---------
 # Initialize tracing and an exporter that can send data to Honeycomb
 provider = TracerProvider()
@@ -36,12 +40,21 @@ provider.add_span_processor(simple_processor)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
+# xray
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+
+
+
 app = Flask(__name__)
 
 # HoneyComb.io
 # -------------------------------
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
+
+# xray
+XRayMiddleware(app, xray_recorder)
 
 frontend = os.getenv('FRONTEND_URL', '*')  # Use '*' as a default if not set
 backend = os.getenv('BACKEND_URL', '*')    # Use '*' as a default if not set
--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -1,12 +1,24 @@
 from datetime import datetime, timedelta, timezone
+from aws_xray_sdk.core import xray_recorder
+
 class UserActivities:
   def run(user_handle):
+    
+    #segment = xray_recorder.begin_segment("user_activities")
+    subsegment = xray_recorder.begin_subsegment("mock_data")
     model = {
       'errors': None,
       'data': None
     }
 
     now = datetime.now(timezone.utc).astimezone()
+    
+    dict = {
+      "now": now.isoformat(),
+      "hello": "there aws xray"
+    }
+    
+    subsegment.put_metadata("key", dict, 'namespace')
 
     if user_handle == None or len(user_handle) < 1:
       model['errors'] = ['blank_user_handle']
@@ -20,4 +32,6 @@ class UserActivities:
         'expires_at': (now + timedelta(days=31)).isoformat()
       }]
       model['data'] = results
+
+    xray_recorder.end_subsegment()
     return model
