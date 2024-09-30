f1da0450ce3d4fbe52765bca66e86556b27e3343
Mikael <mikael.hatanpaa@gmail.com>
2024-09-30 21:42:02 +0200

    Added decoraters for xray, dont know if they are needed so commenting out

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -159,11 +159,13 @@ def data_create_message():
   return
 
 @app.route("/api/activities/home", methods=['GET'])
+#@xray_recorder.capture('activities_home')
 def data_home():
   data = HomeActivities.run()
   return data, 200
 
 @app.route("/api/activities/@<string:handle>", methods=['GET'])
+#@xray_recorder.capture('activities_users')
 def data_handle(handle):
   model = UserActivities.run(handle)
   if model['errors'] is not None:
